fd42292daace58f4c82c77648f42a94768065717
Yonatan Kapash <jonykapash@gmail.com>
2021-03-31 13:12:26 +0300

    add basic styling to contact-form

--- portfolio.js
+++ portfolio.js
@@ -16,21 +16,24 @@ let cities = [
 	"https://maps.google.com/maps?q=ramat%20gan&t=&z=13&ie=UTF8&iwloc=&output=embed",
 	"https://maps.google.com/maps?q=tel%20aviv&t=&z=13&ie=UTF8&iwloc=&output=embed",
 ];
+
 let i = 0;
-document.getElementById("prev_button").disabled = true;
+
+document.getElementById("prev_button").disabled = true; //disable prev button when page loads.
 
 function nextItem() {
 	document.getElementById("prev_button").disabled = false;
 	i++; // increase i by one
 	if (i >= cities.length - 1) {
+		// disable next button at the last city.
 		document.getElementById("next_button").disabled = true;
 	}
 
-	return cities[i]; // give us back the item of where we are now
+	return cities[i]; // give us back the item of where we are now.
 }
 
 function prevItem() {
-	i--; // decrease by one
+	i--; // decrease by one.
 	if (i < cities.length) {
 		document.getElementById("next_button").disabled = false;
 	}
@@ -38,23 +41,22 @@ function prevItem() {
 		document.getElementById("prev_button").disabled = true;
 	}
 
-	return cities[i]; // give us back the item of where we are now
+	return cities[i]; // give us back the item of where we are now.
 }
 
 window.addEventListener("load", function () {
 	document.getElementById("prev_button").addEventListener(
-		"click", // we want to listen for a click
+		"click", // we want to listen for a click.
 		function (e) {
-			// the e here is the event itself
-
+			// the e here is the event itself.
 			document.getElementById("gmap_canvas").setAttribute("src", prevItem());
 		}
 	);
 
 	document.getElementById("next_button").addEventListener(
-		"click", // we want to listen for a click
+		"click", // we want to listen for a click.
 		function (e) {
-			// the e here is the event itself
+			// the e here is the event itself.
 
 			document.getElementById("gmap_canvas").setAttribute("src", nextItem());
 		}
