0444d0269e7361f81e47e683bb455dc7ad713fbf
Yonatan Kapash <jonykapash@gmail.com>
2021-04-01 17:38:37 +0300

    add function to print inputs to the console

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,13 @@
-const GITHUB_URL = "https://api.github.com/users/IsraelTechChallengeITC";
+// const GITHUB_URL = "https://api.github.com/users/IsraelTechChallengeITC";
 
-fetch(GITHUB_URL)
-	.then(function (response) {
-		return response.json();
-	})
-	.then(function (data) {
-		const profileImage = document.getElementById("profile-image");
-		profileImage.src = data.avatar_url;
-	});
+// fetch(GITHUB_URL)
+// 	.then(function (response) {
+// 		return response.json();
+// 	})
+// 	.then(function (data) {
+// 		const profileImage = document.getElementById("profile-image");
+// 		profileImage.src = data.avatar_url;
+// 	});
 
 let cities = [
 	"https://maps.google.com/maps?q=kfar%20saba&t=&z=15&ie=UTF8&iwloc=&output=embed",
@@ -15,6 +15,13 @@ let cities = [
 	"https://maps.google.com/maps?q=kfar%20haruv&t=&z=13&ie=UTF8&iwloc=&output=embed",
 	"https://maps.google.com/maps?q=ramat%20gan&t=&z=13&ie=UTF8&iwloc=&output=embed",
 	"https://maps.google.com/maps?q=tel%20aviv&t=&z=13&ie=UTF8&iwloc=&output=embed",
+];
+
+let checkboxIds = [
+	"JavaScript-checkbox",
+	"ruby-checkbox",
+	"c#-checkbox",
+	"python-checkbox",
 ];
 
 let i = 0;
@@ -62,3 +69,33 @@ window.addEventListener("load", function () {
 		}
 	);
 });
+
+// print the form to the console after pressing submit button
+// let nameInput = document.getElementById("input-name");
+// document
+// 	.querySelector("form")
+// 	.addEventListener("submit", function (e) {
+// 		//prevent the normal submission of the form e.preventDefault();
+// 		console.log(nameInput.value);
+// 	});
+
+function formSubmit() {
+	console.log(
+		`First Name: ${document.getElementById("input-name").value}
+			 
+		 Last Name: ${document.getElementById("input-Lname").value}
+			 
+		 Email: ${document.getElementById("input-email").value}
+			
+		 Comment: ${document.getElementById("input-Comment").value}`
+	);
+
+	for (let n = 0; n < checkboxIds.length; n++) {
+		if (document.getElementById(checkboxIds[n]).checked === true) {
+			console.log(document.getElementById(checkboxIds[n]).value);
+		}
+	}
+
+	let form = document.getElementById("contact-form");
+	form.reset();
+}
