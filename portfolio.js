ac9445434f4cc62820009cf658b5a80a9d4fb002
Yonatan Kapash <jonykapash@gmail.com>
2021-04-25 14:18:53 +0300

    minor changes -cites array

--- portfolio.js
+++ portfolio.js
@@ -14,13 +14,16 @@ fetch(GITHUB_URL)
 //  Google city coordinates for the mini map in about.html
 const kfarSaba =
 	"https://maps.google.com/maps?q=kfar%20saba&t=&z=15&ie=UTF8&iwloc=&output=embed";
-const cities = [
-	kfarSaba, //todo: fix the rest
-	"https://maps.google.com/maps?q=Herzliya&t=&z=13&ie=UTF8&iwloc=&output=embed",
-	"https://maps.google.com/maps?q=kfar%20haruv&t=&z=13&ie=UTF8&iwloc=&output=embed",
-	"https://maps.google.com/maps?q=ramat%20gan&t=&z=13&ie=UTF8&iwloc=&output=embed",
-	"https://maps.google.com/maps?q=tel%20aviv&t=&z=13&ie=UTF8&iwloc=&output=embed",
-];
+const herzliya =
+	"https://maps.google.com/maps?q=Herzliya&t=&z=13&ie=UTF8&iwloc=&output=embed";
+const kfarHaruv =
+	"https://maps.google.com/maps?q=kfar%20haruv&t=&z=13&ie=UTF8&iwloc=&output=embed";
+const ramatGan =
+	"https://maps.google.com/maps?q=ramat%20gan&t=&z=13&ie=UTF8&iwloc=&output=embed";
+const telAviv =
+	"https://maps.google.com/maps?q=tel%20aviv&t=&z=13&ie=UTF8&iwloc=&output=embed";
+
+const cities = [kfarSaba, herzliya, kfarHaruv, ramatGan, telAviv];
 
 //For validation if checkbox has been checked and if so which one
 const checkboxIds = [
@@ -33,54 +36,70 @@ const checkboxIds = [
 // Coding languages for the footer sentence
 const codeLanguages = ["HTML", "CSS", "JavaScript"];
 
-let i = 0;
+let firstName = document.getElementById("input-firstName");
+let email = document.getElementById("input-email");
+let comment = document.getElementById("input-Comment");
 
-function nextItem() {
-	document.getElementById("prev_button").disabled = false;
-	i++; // increase i by one
-	if (i >= cities.length - 1) {
+const animationDuration = 2500;
+
+let citiesIndex = 0;
+
+// loop trow codeLanguages arr and set the footer-sentence
+var e = "";
+for (var y = 0; y < codeLanguages.length; y++) {
+	if (y != codeLanguages.length - 1) {
+		e += `${codeLanguages[y]}, `;
+	} else {
+		e += `and ${codeLanguages[y]}.`;
+	}
+}
+
+document.getElementById(
+	"footer-sentence"
+).innerHTML = `This page was built using: ${e}`;
+
+function enableDisableButtons() {
+	if (citiesIndex <= 0) {
+		// disable previous button at the first city.
+		document.getElementById("prev_button").disabled = true;
+	} else if (0 < citiesIndex && citiesIndex < cities.length - 1) {
+		// if we are in between the start and end of cities arr next and previous button enabled
+		document.getElementById("next_button").disabled = false;
+		document.getElementById("prev_button").disabled = false;
+	} else {
 		// disable next button at the last city.
 		document.getElementById("next_button").disabled = true;
 	}
+}
 
-	return cities[i]; // give us back the item of where we are now
+function nextItem() {
+	citiesIndex++; // increase citiesIndex by one
+	enableDisableButtons();
+	return cities[citiesIndex]; // give us back the item of where we are now
 }
 
 function prevItem() {
-	i--; // decrease by one
-	if (i < cities.length) {
-		document.getElementById("next_button").disabled = false;
-	}
-	if (i <= 0) {
-		// disable previous button at the first city.
-		document.getElementById("prev_button").disabled = true;
-	}
-
-	return cities[i]; // give us back the item of where we are now
+	citiesIndex--; // decrease by one
+	enableDisableButtons();
+	return cities[citiesIndex]; // give us back the item of where we are now
 }
 
+//define google map coordinations event listeners
 window.addEventListener("load", function () {
 	document.getElementById("prev_button").addEventListener(
 		"click", // we want to listen for a click
-		function (e) {
-			// the e here is the event itself
+		function () {
 			document.getElementById("gmap-canvas").setAttribute("src", prevItem());
 		}
 	);
 
 	document.getElementById("next_button").addEventListener(
 		"click", // we want to listen for a click
-		function (e) {
-			// the e here is the event itself
-
+		function () {
 			document.getElementById("gmap-canvas").setAttribute("src", nextItem());
 		}
 	);
 });
-
-let firstName = document.getElementById("input-firstName");
-let email = document.getElementById("input-email");
-let comment = document.getElementById("input-Comment");
 
 // Runs every time a user release a key and check all the required input fields
 function checkIfInputEmpty() {
@@ -95,7 +114,7 @@ function checkIfInputEmpty() {
 	}
 }
 
-// validate input email
+// validate for email input in contact page
 function validateEmail(emailAddress) {
 	var re = /^\S+@\S+\.\S+$/;
 	return re.test(emailAddress);
@@ -118,37 +137,24 @@ function formSubmit() {
 	}
 
 	console.log(submitPrint);
+
 	// reset form values back to empty
-	let form = document.getElementById("contact-form");
-	form.reset();
+	document.getElementById("contact-form").reset();
 
 	// disable the form submit button after form submitted
 	document.getElementById("button-submit").disabled = true;
 }
 
-function activeAnimation() {
+function activateAnimation() {
 	document.getElementById("animate").classList.add("animate__hinge");
 
 	setTimeout(function () {
 		document.getElementById("animate").classList.remove("animate__hinge");
-	}, 2500);
+	}, animationDuration);
 }
-
-// loop trow codeLanguages arr and set the footer-sentence
-var e = "";
-for (var y = 0; y < codeLanguages.length; y++) {
-	if (y != codeLanguages.length - 1) {
-		e += `${codeLanguages[y]}, `;
-	} else {
-		e += `and ${codeLanguages[y]}.`;
-	}
-}
-document.getElementById(
-	"footer-sentence"
-).innerHTML = `This page was built using: ${e}`;
 
 // add a click event on about-card element
 // todo: Check why its stopping the page from loading if not at bottom of page.
 document.getElementById("animate").addEventListener("click", function () {
-	activeAnimation();
+	activateAnimation();
 });
